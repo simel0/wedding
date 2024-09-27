ddd10da76717b5ed344a96c711789685d710d063
simel0 <simel0@users.noreply.github.com>
2024-09-27 02:16:30 +0000

    deploy: 324bd8e446b9130e31aea754797a31775535e779

--- common/emoji-picker/index.js
+++ common/emoji-picker/index.js
@@ -1,4 +1,1 @@
-/*https://github.com/phiphi1992/emoji-picker-element*/
-import Picker from './picker.js'
-import Database from './database.js'
-export { Picker, Database }
+import Picker from"./picker.js";import Database from"./database.js";export{Picker,Database};
